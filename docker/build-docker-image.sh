ce0508804c4d25bd510cf17de1dde3122fab2eb2
Dwi Agustianto <dwi.agustianto@ralali.com>
2021-02-10 16:42:19 +0700

    modified: docker image

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-set -u
+# #!/bin/bash
+# set -u
 
-# variables
-REVISION=$CIRCLE_SHA1
+# # variables
+# REVISION=$CIRCLE_SHA1
 
-docker_tag_args=""
-IFS="," read -ra DOCKER_TAGS \<< "$REVISION"
-if [ $CIRCLE_BRANCH = 'development' ]; then
-  for tag in "${DOCKER_TAGS[@]}"; do
-    docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-dev-${SERVICE_NAME}:$tag"
-  done
-  docker build -f docker/Dockerfile-dev $docker_tag_args .
-fi
-if [ $CIRCLE_BRANCH = 'staging' ]; then
-  for tag in "${DOCKER_TAGS[@]}"; do
-    docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-stg-${SERVICE_NAME}:$tag"
-  done
-  docker build -f docker/Dockerfile-stg $docker_tag_args .
-fi
-if [ $CIRCLE_BRANCH = 'master' ]; then
-  for tag in "${DOCKER_TAGS[@]}"; do
-    docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-prd-${SERVICE_NAME}:$tag"
-  done
-  docker build -f docker/Dockerfile-prd $docker_tag_args .
-fi
+# docker_tag_args=""
+# IFS="," read -ra DOCKER_TAGS \<< "$REVISION"
+# if [ $CIRCLE_BRANCH = 'development' ]; then
+#   for tag in "${DOCKER_TAGS[@]}"; do
+#     docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-dev-${SERVICE_NAME}:$tag"
+#   done
+#   docker build -f docker/Dockerfile-dev $docker_tag_args .
+# fi
+# if [ $CIRCLE_BRANCH = 'staging' ]; then
+#   for tag in "${DOCKER_TAGS[@]}"; do
+#     docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-stg-${SERVICE_NAME}:$tag"
+#   done
+#   docker build -f docker/Dockerfile-stg $docker_tag_args .
+# fi
+# if [ $CIRCLE_BRANCH = 'master' ]; then
+#   for tag in "${DOCKER_TAGS[@]}"; do
+#     docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-prd-${SERVICE_NAME}:$tag"
+#   done
+#   docker build -f docker/Dockerfile-prd $docker_tag_args .
+# fi
