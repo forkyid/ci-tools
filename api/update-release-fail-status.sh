2264a7cb3dcd03a9448b1c68fbeb2320adf0a24b
Dwi Agustianto <dwi.agustianto@ralali.com>
2021-02-10 17:48:00 +0700

    modified: api release status

--- api/update-release-fail-status.sh
+++ api/update-release-fail-status.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if [ $CIRCLE_BRANCH = 'development' ]; then
-  curl --location --request PATCH $API_DEV_RELEASES_STATUS --header "Authorization: Basic $BASIC_AUTH" --header 'Content-Type: application/json' --data-raw "{\"workflow_name\": \"$WORKFLOW_NAME\", \"workflow_id\": \"$CIRCLE_WORKFLOW_ID\", \"job_id\": \"$CIRCLE_WORKFLOW_JOB_ID\", \"stage\": \"development\", \"project_name\": \"$SERVICE_NAME\", \"release_status\": \"failed\"}"
-fi
-if [ $CIRCLE_BRANCH = 'staging' ]; then
-  curl --location --request PATCH $API_STG_RELEASES_STATUS --header "Authorization: Basic $BASIC_AUTH" --header 'Content-Type: application/json' --data-raw "{\"workflow_name\": \"$WORKFLOW_NAME\", \"workflow_id\": \"$CIRCLE_WORKFLOW_ID\", \"job_id\": \"$CIRCLE_WORKFLOW_JOB_ID\", \"stage\": \"staging\", \"project_name\": \"$SERVICE_NAME\", \"release_status\": \"failed\"}"
-fi
-if [ $CIRCLE_BRANCH = 'master' ]; then
-  curl --location --request PATCH $API_PRD_RELEASES_STATUS --header "Authorization: Basic $BASIC_AUTH" --header 'Content-Type: application/json' --data-raw "{\"workflow_name\": \"$WORKFLOW_NAME\", \"workflow_id\": \"$CIRCLE_WORKFLOW_ID\", \"job_id\": \"$CIRCLE_WORKFLOW_JOB_ID\", \"stage\": \"master\", \"project_name\": \"$SERVICE_NAME\", \"release_status\": \"failed\"}"
-fi
+STAGE=""
+API=""
+
+case $CIRCLE_BRANCH in
+development)
+  STAGE="dev"
+  API=$API_DEV_RELEASES_STATUS
+  break
+  ;;
+staging)
+  STAGE="stg"
+  API=$API_STG_RELEASES_STATUS
+  break
+  ;;
+production)
+  STAGE="prd"
+  API=$API_PRD_RELEASES_STATUS
+  break
+  ;;
+esac
+
+curl --location --request PATCH $API --header "Authorization: Basic $BASIC_AUTH" --header 'Content-Type: application/json' --data-raw "{\"workflow_name\": \"$WORKFLOW_NAME\", \"workflow_id\": \"$CIRCLE_WORKFLOW_ID\", \"job_id\": \"$CIRCLE_WORKFLOW_JOB_ID\", \"stage\": \"$STAGE\", \"project_name\": \"$SERVICE_NAME\", \"release_status\": \"failed\"}"
