2f840d31a560ad2e5b09e0bc93ffbf2d75154002
Dwi Agustianto <dwi.agustianto@ralali.com>
2021-02-10 16:52:38 +0700

    modified: push docker-image

--- docker/push-docker-image.sh
+++ docker/push-docker-image.sh
@@ -3,20 +3,22 @@ set -u
 
 REVISION=$CIRCLE_SHA1
 IFS="," read -ra DOCKER_TAGS <<< "$REVISION"
-
+docker_tag_args=""
 if [ $CIRCLE_BRANCH = 'development' ]; then
   for tag in "${DOCKER_TAGS[@]}"; do
     docker_tag_args="$docker_tag_args -t $AWS_ECR_ACCOUNT_URL/sgg-dev-${SERVICE_NAME}:$tag"
   done
   # echo docker_tag_args
   docker build -f docker/Dockerfile-dev $docker_tag_args .
-fi
 
-if [ $CIRCLE_BRANCH = 'development' ]; then
   for tag in "${DOCKER_TAGS[@]}"; do
     docker push $AWS_ECR_ACCOUNT_URL/sgg-dev-${SERVICE_NAME}:${tag}
   done
 fi
+
+# if [ $CIRCLE_BRANCH = 'development' ]; then
+
+# fi
 # if [ $CIRCLE_BRANCH = 'staging' ]; then
 #   for tag in "${DOCKER_TAGS[@]}"; do
 #     docker push $AWS_ECR_ACCOUNT_URL/sgg-stg-${SERVICE_NAME}:${tag}
