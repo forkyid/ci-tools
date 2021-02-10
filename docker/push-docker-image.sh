5880ae0b4f6a2ca35f505399d34bcff77f106895
Dwi Agustianto <dwi.agustianto@ralali.com>
2021-02-10 16:27:35 +0700

    modified: push docker image

--- docker/push-docker-image.sh
+++ docker/push-docker-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+REVISION=$CIRCLE_SHA1
+IFS="," read -ra DOCKER_TAGS \<< "$REVISION"
 
-IFS="," read -ra DOCKER_TAGS \<< "$CIRCLE_SHA1"
 if [ $CIRCLE_BRANCH = 'development' ]; then
   for tag in "${DOCKER_TAGS[@]}"; do
     docker push $AWS_ECR_ACCOUNT_URL/sgg-dev-${SERVICE_NAME}:${tag}
